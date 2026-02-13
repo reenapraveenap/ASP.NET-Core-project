0482471719f34760432ff5ed18223addd29d9f65
reenapraveenap <reenapraveena2019@gmail.com>
2026-02-13 10:13:11 +0500

    Initial commit

---
+++ View/ProtectionPDF.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ProtectionPDF.aspx.cs" Inherits="FileProtection.View.ProtectionPDF" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:GridView ID="grdStudents" runat="server" AutoGenerateColumns="true"></asp:GridView>
+            <%--            <asp:Button runat="server" ID="btnGetDetails" Text="btnGetDetails" OnClick="btnGetDetails_Click" />--%>
+            <br />
+           <asp:Button ID="btnExport" runat="server" Text="Export To PDF" OnClick = "ExportToPDF" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ View/ProtectionPDF.aspx.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using iText.Kernel.Pdf;
+using iText.Layout;
+using iText.Layout.Element;
+
+
+namespace FileProtection.View
+{
+    public partial class ProtectionPDF : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!IsPostBack)
+            {
+                this.BindGrid();
+            }
+        }
+        private void BindGrid()
+        {
+            string strConnString = ConfigurationManager.ConnectionStrings["conn_resgisteredASP"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(strConnString))
+            {
+                using (SqlCommand cmd = new SqlCommand("getStudentDetails"))
+                {
+                    using (SqlDataAdapter sda = new SqlDataAdapter())
+                    {
+                        cmd.Connection = con;
+                        sda.SelectCommand = cmd;
+                        using (DataTable dt = new DataTable())
+                        {
+                            sda.Fill(dt);
+                            grdStudents.DataSource = dt;
+                            grdStudents.DataBind();
+                        }
+                    }
+                }
+            }
+        }
+
+        protected void ExportToPDF(object sender, EventArgs e)
+        {
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                PdfWriter writer = new PdfWriter(memoryStream);
+                PdfDocument pdfDoc = new PdfDocument(writer);
+                Document doc = new Document(pdfDoc);
+
+                //To Export all pages
+                grdStudents.AllowPaging = false;
+                this.BindGrid();
+
+                foreach (var row in grdStudents.Rows)
+                {
+                    foreach (var cell in ((GridViewRow)row).Cells)
+                    {
+                        doc.Add(new Paragraph(cell.Text));
+                    }
+                }
+
+                pdfDoc.Close();
+
+                byte[] bytes = memoryStream.ToArray();
+                memoryStream.Close();
+
+                using (MemoryStream output = new MemoryStream())
+                {
+                    string password = "pass@123";
+                    PdfReader reader = new PdfReader(bytes);
+                    PdfWriter encryptedWriter = new PdfWriter(output,
+                        new WriterProperties().SetStandardEncryption(
+                            new PdfWriter.EncryptionProperties().SetPassword(password),
+                            new PdfReader.EncryptionProperties().AddStandardPermissions(PdfName.Printing)));
+
+                    PdfDocument encryptedDoc = new PdfDocument(reader, encryptedWriter);
+
+                    encryptedDoc.Close();
+
+                    bytes = output.ToArray();
+
+                    Response.ContentType = "application/pdf";
+                    Response.AddHeader("content-disposition", "attachment;filename=GridViewExport.pdf");
+                    Response.Cache.SetCacheability(HttpCacheability.NoCache);
+                    Response.BinaryWrite(bytes);
+                    Response.End();
+                }
+            }
+        }
+
+        public override void VerifyRenderingInServerForm(Control control)
+        {
+            /* Verifies that the control is rendered */
+        }
+
+       
+    }
+}
